9a7804ec174b959a1eb555cfc0fa55b051260d4e
曹冠雄 <1120643432@qq.com>
2020-11-04 14:27:27 +0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -14,6 +14,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f https://github.com/Fulu5/qr-code.git master:gh-pages
+git push -f https://github.com/Fulu5/qr-code.git master
 
 cd -
